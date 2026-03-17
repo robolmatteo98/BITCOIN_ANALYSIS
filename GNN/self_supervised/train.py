cfa6bd20e0293f1bcea1c9282a23008b01b6912e
matteo.robol <matteo.robol@niris.eu>
2026-03-17 17:39:25 +0100

    fix train

--- GNN/self_supervised/train.py
+++ GNN/self_supervised/train.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn.functional as F
 
+# funzione di separazione train e test a seconda se si trova sotto o sopra la media delle transazioni nella quale è coinvolto quell'indirizzo
 def temporal_edge_split(data, cutoff):
     train_mask = data.edge_time < cutoff
     test_mask = data.edge_time >= cutoff
@@ -16,26 +17,25 @@ def train_model(model, data):
 
     model.train()
 
-    # split temporale (usiamo la mediana come cutoff)
+    # split temporale (mediana come cutoff)
     cutoff = data.edge_time.median()
     train_edges, _ = temporal_edge_split(data, cutoff)
 
     for _ in range(200):
-        optimizer.zero_grad()
+        optimizer.zero_grad() # azzera i gradienti
 
-        # embedding nodi
+        # z è un embedding, ovvero un vettore contenente tutte le caratteristiche del nodo nello spazio latente
         z = model(data.x, train_edges)
 
-        # positive edges
+        # positive edges --> se due nodi sono connessi, il loro prodotto scalare sarà alto; se non lo sono, basso
         pos_src = train_edges[0]
         pos_dst = train_edges[1]
         pos_score = (z[pos_src] * z[pos_dst]).sum(dim=1)
 
-        # negative sampling
+        # negative sampling --> per ogni edge positivo viene generato un edge negativo casuale (nodi non connessi), così vengono soddisfatti anche gli esempi negativi
         num_nodes = data.num_nodes
         neg_src = torch.randint(0, num_nodes, pos_src.size())
         neg_dst = torch.randint(0, num_nodes, pos_dst.size())
-
         neg_score = (z[neg_src] * z[neg_dst]).sum(dim=1)
 
         # loss BCE
@@ -47,8 +47,8 @@ def train_model(model, data):
             ])
         )
 
-        loss.backward()
-        optimizer.step()
+        loss.backward() # calcola i gradienti della loss rispetto ai pesi del modello
+        optimizer.step() # aggiorna i pesi secondo Adam usando i gradienti calcolati
 
     return model
 
